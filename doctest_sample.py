c415680ffa3fb7cf5f921155c3e2a45adfd3d513
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-03-01 17:44:22 +0330

    feat:add sample doctest

---
+++ doctest_sample.py
@@ -0,0 +1,34 @@
+"""
+>>> addxy(2,3)
+5
+
+
+>>> addxy(4,-5)
+-1
+
+>>> subb(3,4)
+-1
+>>> subb(4,-5)
+9
+
+
+
+>>> multip(4,5)
+20
+
+>>> multip(5,6)
+30
+
+"""
+
+
+def addxy(x, y):
+    return x + y
+
+
+def subb(x, y):
+    return x - y
+
+
+def multip(x, y):
+    return x * y
